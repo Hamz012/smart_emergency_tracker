9f96223672c81ec52ccd77f34a25fbc296f55b90
Ilham <ilhamsiddik30@gmail.com>
2026-05-12 18:29:36 +0700

    masi pagi ngentoddd

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,18 +1,22 @@
 <?php
 
+$host = $_ENV['MYSQLHOST'] ?? getenv('MYSQLHOST');
+$user = $_ENV['MYSQLUSER'] ?? getenv('MYSQLUSER');
+$pass = $_ENV['MYSQLPASSWORD'] ?? getenv('MYSQLPASSWORD');
+$db   = $_ENV['MYSQLDATABASE'] ?? getenv('MYSQLDATABASE');
+$port = $_ENV['MYSQLPORT'] ?? getenv('MYSQLPORT');
+
 $conn = mysqli_connect(
-
-    getenv('MYSQLHOST'),
-    getenv('MYSQLUSER'),
-    getenv('MYSQLPASSWORD'),
-    getenv('MYSQLDATABASE'),
-    getenv('MYSQLPORT')
-
+    $host,
+    $user,
+    $pass,
+    $db,
+    (int)$port
 );
 
 if (!$conn) {
 
-    die('Koneksi Database Gagal : ' . mysqli_connect_error());
+    die('Koneksi database gagal: ' . mysqli_connect_error());
 
 }
 
